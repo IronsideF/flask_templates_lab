9142835587f37f8cf2049423cb5b8978f134e1b9
Nathaniel Forsyth <ironsideforsyth@gmail.com>
2022-07-07 13:56:41 +0100

    mvp achieved

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from app import app
 from flask import render_template, request, redirect
 from models.event_list import *
@@ -6,3 +7,14 @@ from models.event_list import *
 @app.route('/events')
 def index():
     return render_template('index.html', title='Event List', all_events=event_list)
+
+@app.route('/events', methods=['POST'])
+def add_event():
+    date = request.form['date']
+    name = request.form['name']
+    guest_num = request.form['guest_num']
+    room = request.form['room']
+    desc = request.form['desc']
+    new_event = Event(date, name, guest_num, room, desc)
+    add_new_event(new_event)
+    return redirect('/events')
--- models/event_list.py
+++ models/event_list.py
@@ -4,5 +4,8 @@ event_1 = Event('21/06/2019', "Keith's Birthday", 15, '1', 'A birthday bash for 
 event_2 = Event('30/09/2007', "Sky's Graduation", 73, '9', 'Sky graduates college')
 event_3 = Event('23/12/2021', "Ben's Christmas Party", 3, '4', 'An intimate gathering for christmas')
 
-event_list = [event_1, event_2, event_3]    
+event_list = [event_1, event_2, event_3]
+
+def add_new_event(event):
+    event_list.append(event)
 
